be22a193d0a156abd050c83d4982f7599a57b985
Poovarasi <jpoovarasi7@gmail.com>
2026-06-20 20:24:00 +0530

    Time: 11 ms (91.25%), Space: 14.3 MB (11.95%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        clean_s = ""
+        if s==" ":
+            return True
+        clean_s = "".join(char.lower() for char in s if char.isalnum())
+        return clean_s == clean_s[::-1]
