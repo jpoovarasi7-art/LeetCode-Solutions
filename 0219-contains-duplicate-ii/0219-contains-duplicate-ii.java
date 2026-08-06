5aca78058d7eb9904fdb3deaa2785d3464a5e999
Poovarasi <jpoovarasi7@gmail.com>
2026-08-06 11:18:42 +0530

    Time: 23 ms (85.88%), Space: 95 MB (27.77%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> window = new HashSet<>();      
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                window.remove(nums[i - k - 1]);
+            }
+            if (!window.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
